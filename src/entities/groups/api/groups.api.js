dfa2e5464179c84ddc8c4b66324b304bd520515d
Dmitriy_Zakirov <metrio@inbox.ru>
2023-11-16 17:08:35 +0500

    newRequest2.0

--- src/entities/groups/api/groups.api.js
+++ src/entities/groups/api/groups.api.js
@@ -3,13 +3,29 @@ import { baseApi } from '../../../app/baseApi';
 export const groupsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getGroups: builder.query({
-      query: () => '/groups',
+      query: () => '/api/groups',
     }),
     getMyGroups: builder.query({
-      query: () => '/my-groups',
+      query: () => '/api/my-groups',
     }),
     getBooks: builder.query({
       query: () => '/todos',
+    }),
+    getGroupsId: builder.query({
+      query: (id) => `/api/groups/${id}`,
+    }),
+    getStudentsId: builder.query({
+      query: (id) => `/api/students/${id}`,
+    }),
+    changeStudent: builder.mutation({
+      query: (id, body) => ({
+        body: body,
+        url: `/api/students/${id}`,
+        method: 'PUT',
+      }),
+    }),
+    getStudentsIdHistory: builder.query({
+      query: (id) => `/api/students/${id}/history`,
     }),
   }),
   overrideExisting: false,
